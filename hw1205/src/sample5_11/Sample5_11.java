b2195fddc43ab8d6732731546db28524ed50362d
abbyhuang0726 <abbyhuang0726@gmail.com>
2019-12-01 18:37:46 +0800

    first commit

---
+++ hw1205/src/sample5_11/Sample5_11.java
@@ -0,0 +1,15 @@
+package sample5_11;
+import java.io.*;
+public class Sample5_11 {
+	public static void main(String[]args) {
+		System.out.println("請輸入字串");
+		try {
+			BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+			String str=br.readLine();
+			System.out.println("已輸入字串"+str);
+		}
+		catch(IOException e) {
+			System.out.println("輸入輸出有誤");
+		}
+	}
+}
---
+++ hw1205/src/sample5_12/Sample5_12.java
@@ -0,0 +1,17 @@
+package sample5_12;
+import java.io.*;
+public class Sample5_12 {
+	public static void main(String[]args) {
+		try {
+			FileWriter fw=new FileWriter("test1.txt");
+			PrintWriter pw=new PrintWriter(new BufferedWriter(fw));
+			pw.println("Hello!");
+			pw.println("GoodBye!");
+			pw.close();
+			System.out.println("資料已寫入檔案了");
+		}
+		catch(IOException e) {
+			System.out.println("輸出入錯誤");
+		}
+	}
+}
